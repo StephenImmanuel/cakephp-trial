6a86d6e4d92994dc9ff47c2344db589fc8bf1a5e
StephenImmanuel <arputharajstephen@gmail.com>
2021-02-07 10:37:44 +0800

    Pop Up to mouse over

--- app/Controller/FormatController.php
+++ app/Controller/FormatController.php
@@ -3,12 +3,24 @@
 		
 		public function q1(){
 			
-			$this->setFlash('Question: Please change Pop Up to mouse over (soft click)');
-				
+            $this->setFlash('Question: Please change Pop Up to mouse over (soft click)');
+            
+            if ($this->request->is('POST')){
+                $this->redirect(
+                    array(
+                        'action' => 'show',
+                        'type' =>  $this->request->data['Type']['type']
+                    )
+                );
+            }	
 			
 // 			$this->set('title',__('Question: Please change Pop Up to mouse over (soft click)'));
 		}
-		
+        
+        public function show()
+        {
+        }
+        
 		public function q1_detail(){
 
 			$this->setFlash('Question: Please change Pop Up to mouse over (soft click)');
---
+++ app/View/Format/show.ctp
@@ -0,0 +1,6 @@
+
+<div class="alert alert-info">
+    <h3>Format View</h3>
+</div>
+
+<?php echo "Selected type is: ".$this->request->params['named']['type']; ?>
